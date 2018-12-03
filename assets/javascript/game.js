be10d39151e980340283ca67be68ccac11314da5
Rane M Kaseman <raykman@comcast.net>
2018-12-02 18:46:53 -0700

    more functionality 1846

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,70 @@
 
 var letters = ["a", "e", "i", "o", "u"];
 
-var targetLetter = letters[Math.floor(Math.random() * letters.length)];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 2;
-var guessedLetters = [];
-console.log("targetLetter = " + targetLetter);
+var guessArray = [];
 console.log("wins = " + wins);
 console.log("losses = " + losses);
 console.log("guessesLeft = " + guessesLeft);
-console.log("guessedLetters = " + guessedLetters);
+console.log("guessArray = " + guessArray);
+
+gameSetup();
 
 function gameSetup() {
+    targetLetter();
     document.querySelector("#wins").innerHTML = "Wins: " + wins;
     document.querySelector("#losses").innerHTML = "Losses: " + losses;
     document.querySelector("#left").innerHTML = "Guesses left: " + guessesLeft;
-    document.querySelector("#used").innerHTML = "Guesses made: " + guessedLetters;
+    guessArray = [];
+    document.querySelector("#used").innerHTML = "Your guesses: " + guessArray;
 }
 
-// gameSetup();
-
-function subtractGuessesLeft() {
-    guessesLeft - 1;
-    document.querySelector("#left").innerHTML = "Guesses left: " + guessesLeft;
-};
+function targetLetter() {
+    pick = letters[Math.floor(Math.random() * letters.length)];
+    console.log("pick function = " + pick);
+    return pick;
+}
 
 function addGuessedLetters() {
-    document.querySelector("#used").innerHTML = "Your guesses: " + guessedLetters;
+    document.querySelector("#used").innerHTML = "Your guesses: " + guessArray;
 };
 
-// subtractGuessesLeft();
-
-document.onkeyup = function(keyPress) {
-    // console.log(keyPress);
-    guessesLeft - 1;
+document.onkeyup = function (keyPress) {
     console.log("guessesLeft = " + guessesLeft);
     document.querySelector("#left").innerHTML = "Guesses left: " + guessesLeft;
     var userGuess = String.fromCharCode(keyPress.keyCode).toLowerCase();
     console.log("userGuess = " + userGuess);
+    guessArray.push(userGuess);
+    addGuessedLetters();
+    console.log("guessArray = " + guessArray);
 
     if (guessesLeft > 0) {
-        // if (userGuess !== targetLetter) {
-        //     console.log("targetLetter = " + targetLetter);
-        //     guessedLetters.push(userGuess);
-        //     subtractGuessesLeft();
+        // if (userGuess !== pick) {
+        //     console.log("pick = " + pick);
+        //     guessArray.push(userGuess);
+        //     guessCount();
         //     addGuessedLetters();
-        //     console.log("guessedLetters = " + guessedLetters);
+        //     console.log("guessArray = " + guessArray);
         // }
-            if (userGuess === targetLetter) {
+        if (userGuess === pick) {
             wins++;
             document.querySelector("#wins").innerHTML = "Wins: " + wins;
-            document.querySelector("#message").innerHTML = "you win";
-            subtractGuessesLeft();
+            document.querySelector("#message").innerHTML = "Win <em>!!</em> (but you're not psychic).";
+            guessesLeft--;
+            document.querySelector("#left").innerHTML = "Guesses left: " + guessesLeft;
+            gameSetup();
+            console.log("pick function win = " + pick);
+            console.log("wins = " + wins);
+        } else if (guessesLeft === 0 && userGuess !== pick) {
+            losses++;
+            document.querySelector("#losses").innerHTML = "Losses: " + losses;
+            document.querySelector("#message").innerHTML = "Lose. You're not psychic.";
+            gameSetup();
+            console.log("pick function lose = " + pick);
+            console.log("losses = " + losses);
         }
-        // if (userGuess !== targetLetter) {
-        //     losses++;
-        //     document.querySelector("#message").innerHTML = "Losses: " + losses;
-        //     document.querySelector("#message").innerHTML = "you lose";
-        // }
         // } else if (guessesLeft === 0) {
         //     losses++;
         //     document.querySelector("#message").innerHTML = "Losses: " + losses;
